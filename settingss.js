1be089b9f24b2c6b2c76ddd0c2867c8a7017e11a
Voiitxna102 <godmemory92@gmail.com>
2025-07-23 23:28:36 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~xvkyDThC#ATEXNK2eJhW2ieTf0KORl5jK7GdldRKx81vT9JxnBBo",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "27661021575", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
